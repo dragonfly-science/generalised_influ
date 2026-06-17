aef40720a607f11c8ea2b66eb50b8d4b9612fb33
OxanaHart <osmoliychuk@gmail.com>
2026-06-17 13:35:40 +1200

    add default palette

---
+++ R/utils.R
@@ -0,0 +1,8 @@
+default_palette <- c(
+  'current'  = 'dodgerblue1', 
+  'previous' = '#E41A1CCC', 
+  'main'     = 'black', 
+  'grid'     = 'grey90', 
+  'band'     = 'grey80', 
+  'helper'   = 'black'
+)
